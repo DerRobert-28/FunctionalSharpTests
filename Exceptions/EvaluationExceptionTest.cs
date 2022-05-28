8d1da60f9e89ac28f58fa142ade214a2e9d4b909
Der Robert <eriktainment@gmx.net>
2022-05-28 21:09:03 +0200

    feat: initial commit

---
+++ Exceptions/EvaluationExceptionTest.cs
@@ -0,0 +1,20 @@
+namespace DerRobert.FunctionalSharp.Tests.Exceptions {
+
+	using DerRobert.FunctionalSharp.Exceptions;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+	[TestClass]
+	public class EvaluationExceptionTest: Exceptions {
+
+		[TestMethod("Test of throwing a 'EvaluationException'")]
+		public void testEvaluationException() {
+			testMethod("Test of throwing a 'EvaluationException'");
+			arrangeException(new EvaluationException("throwing EvaluationException"));
+			actException();
+			assertException();
+		}
+
+	}
+
+}
---
+++ Exceptions/MappingExceptionTests.cs
@@ -0,0 +1,20 @@
+namespace DerRobert.FunctionalSharp.Tests.Exceptions {
+
+	using DerRobert.FunctionalSharp.Exceptions;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+	[TestClass]
+	public class MappingExceptionTests: Exceptions {
+
+		[TestMethod("Test of throwing a 'MappingException'")]
+		public void testMappingException() {
+			testMethod("Test of throwing a 'MappingException'");
+			arrangeException(new MappingException("throwing MappingException"));
+			actException();
+			assertException();
+		}
+		
+	}
+
+}
