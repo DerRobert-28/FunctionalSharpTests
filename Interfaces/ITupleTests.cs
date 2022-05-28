ba852fc8924ad8fee5a896c656e2afb609367d4a
Der Robert <eriktainment@gmx.net>
2022-05-28 23:32:45 +0200

    feat: add test class&method

---
+++ Interfaces/ITupleTests.cs
@@ -0,0 +1,18 @@
+﻿namespace DerRobert.FunctionalSharpTests.Interfaces {
+	using DerRobert.FunctionalSharp.Interfaces;
+	using DerRobert.FunctionalSharp.Types.Tuples;
+	using DerRobert.FunctionalSharpTests.Core;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[TestClass]
+	public class ITupleTests: TestClass {
+
+		[TestMethod]
+		public void testMaxArityShouldBeNine() {
+			testMethod("Test if MAX_ARITY is available with value of 9");
+			ITuple t0 = Tuple0.of();
+			assertEquals(t0.MAX_ARITY, 9, "MAX_ARITIY should be 9");
+		}
+
+	}
+}
