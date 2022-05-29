94b4221008f5468bbf70507455665b1a6a5b5bfb
Der Robert <eriktainment@gmx.net>
2022-05-29 21:05:35 +0200

    feat: add expectation type test

--- Tests/Exceptions/EvaluationExceptionTests.cs
+++ Tests/Exceptions/EvaluationExceptionTests.cs
@@ -11,12 +11,11 @@ namespace DerRobert.FunctionalSharpTests.Tests.Exceptions {
 		private const string reason = "Test Automation: EvaluationException";
 		private readonly World evaluationWorld;
 
-		public EvaluationExceptionTests() {
-			evaluationWorld = new World();
-		}
+		public EvaluationExceptionTests() => evaluationWorld = new World();
 		
+
 		[TestMethod]
-		public void testEvaluationException() {
+		public void test_EvaluationException_shouldeBeThrown() {
 			testMethod(reason);
 
 			Arrange
@@ -39,16 +38,36 @@ namespace DerRobert.FunctionalSharpTests.Tests.Exceptions {
 					.exceptionTypeIsNotNull("No exception type has been defined.");
 		}
 
-		[TestMethod]
-		public void testEvaluationExceptionTypeShouldBeCorrect() {
-			testMethod(reason);
-			arrangeException(EvaluationException.because(reason));
-			actException();
-			assertForExceptionType();
-		}
 
 		[TestMethod]
-		public void testExceptionMessageShouldBeCorrect() {
+		public void test_EvaluationExceptionType_shouldBeCorrect() {
+			testMethod(reason);
+
+			Arrange
+				.theWorld(evaluationWorld)
+				.withLogging(this)
+				.withException(EvaluationException.because(reason))
+				.withReason(reason)
+				.withType(typeof(EvaluationException));
+
+			Act
+				.inWorld(evaluationWorld)
+				.withLogging(this)
+				.throwException();
+
+			Expect
+				.forWorld(evaluationWorld)
+				.withLogging(this)
+				.That
+					.anyExceptionIsThrown("Expected exception has not been thrown.")
+					.genericExceptionTypeEqualTo(
+						typeof(EvaluationException),
+						"Exception type should be 'EvaluationException'.");
+		}
+
+
+		[TestMethod]
+		public void test_ExceptionMessage_shouldBeCorrect() {
 			testMethod(reason);
 
 			Arrange
